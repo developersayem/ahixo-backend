2892c11c112c42ab8a0fb5abf24a8c447a992d08
Sayem Molla <developersayem@outlook.com>
2025-10-30 18:34:57 +0600

    admin register

--- server/src/controller/admin/auth.controller.ts
+++ server/src/controller/admin/auth.controller.ts
@@ -11,10 +11,6 @@ import { generateAccessTokenAndRefreshToken } from "../../helper/generateAccessT
 export const adminRegistrationController = asyncHandler(async (req: Request, res: Response) => {
   const { fullName, email, password, phone, shopName, shopAddress } = req.body;
 
-  if ([fullName, email, password, phone].some((field) => !field || field.trim() === "")) {
-    throw new ApiError(400, "All fields are required");
-  }
-
   if (!email.includes("@")) {
     throw new ApiError(400, "Invalid email address");
   }
